0b34e7b82a5a59113f0aa7a5cc3fa5ee50b3fa5d
hoaug-tran <92006941+hoaug-tran@users.noreply.github.com>
2025-09-21 14:30:05 +0700

    feat: add more code language

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -5,11 +5,7 @@ const noteSchema = new mongoose.Schema(
     title: { type: String, required: true, trim: true },
     slug: { type: String, required: true, unique: true, index: true },
     content: { type: String, required: true },
-    language: {
-      type: String,
-      enum: ["python", "java", "other"],
-      default: "other",
-    },
+    language: { type: String, default: "other" },
     tags: [{ type: String }],
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
